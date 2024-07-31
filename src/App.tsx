4f84020dd7cbcee1fa490848f732b3365f36692e
Joel-Sousa <softmock@gmail.com>
2024-07-31 09:31:47 -0300

    Ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -18,47 +18,47 @@ function App() {
   const [brl1, setBrl1] = useState('' as any);
 
   useEffect(() => {
-    
-      if (!brl) setBrl(0);
-      if (!sats1) setSats1(0);
 
-      fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCBRL')
-        .then((response) => response.json())
-        .then((data) => {
+    if (!brl) setBrl(0);
+    if (!sats1) setSats1(0);
 
-          // sats
-          const price = data.price / Math.pow(10, 3);
+    fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCBRL')
+      .then((response) => response.json())
+      .then((data) => {
 
-          setCotacaoFormatado(price.toFixed(3))
-          setCotacao(price.toFixed(8))
+        // sats
+        const price = data.price / Math.pow(10, 3);
 
-          const btcConvert = parseInt(brl) / parseFloat(data.price)
+        setCotacaoFormatado(price.toFixed(3))
+        setCotacao(price.toFixed(8))
 
-          setBtc(btcConvert.toFixed(8))
+        const btcConvert = parseInt(brl) / parseFloat(data.price)
 
-          const satsConvert = (brl / parseFloat(data.price)) * Math.pow(10, 8)
+        setBtc(btcConvert.toFixed(8))
 
-          setSats(maskNumber(satsConvert.toFixed(0)))
+        const satsConvert = (brl / parseFloat(data.price)) * Math.pow(10, 8)
 
-          // brl
-          const valorEmBTC = sats1 / 100000000;
-          const valorEmBRL = valorEmBTC * cotacaoFormatado;
-          setBrl1((valorEmBRL * 1000).toFixed(2));
+        setSats(maskNumber(satsConvert.toFixed(0)))
 
-          const btcConvert1 = parseInt(brl1) / parseFloat(data.price)
-          setBtc1(btcConvert1.toFixed(8))
-        })
+        // brl
+        const valorEmBTC = sats1 / 100000000;
+        const valorEmBRL = valorEmBTC * cotacaoFormatado;
+        setBrl1((valorEmBRL * 1000).toFixed(2));
 
-      const now = new Date();
-      const year = now.getFullYear();
-      const month = now.getMonth() + 1; // Os meses começam em 0
-      const day = now.getDate();
-      const hours = now.getHours();
-      const minutes = now.getMinutes();
-      // const seconds = now.getSeconds();
+        const btcConvert1 = parseInt(brl1) / parseFloat(data.price)
+        setBtc1(btcConvert1.toFixed(8))
+      })
 
-      const timer = day + '/' + month + '/' + year + ' | ' + hours + ':' + minutes;
-      setTime(timer)
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth() + 1; // Os meses começam em 0
+    const day = now.getDate();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    // const seconds = now.getSeconds();
+
+    const timer = day + '/' + month + '/' + year + ' | ' + hours + ':' + minutes;
+    setTime(timer)
 
   }, [cotacaoFormatado, cotacao,
     btc, sats, brl,
@@ -75,7 +75,7 @@ function App() {
   }
 
   return (
-    <div /* style={{marginBottom: '1000px'}} */>
+    <div >
       <div className={styles.section}>
         <TextField
           className={styles.color}
@@ -85,7 +85,16 @@ function App() {
           value={cotacaoFormatado}
           InputLabelProps={{ shrink: true }}
         />
-        | {time}
+        &nbsp;
+        <TextField
+          className={styles.color}
+          label="Data/Hora"
+          variant="filled"
+          type='text'
+          value={time}
+          InputLabelProps={{ shrink: true }}
+        />
+
       </div>
       <div className={styles.section}>
         <TextField
